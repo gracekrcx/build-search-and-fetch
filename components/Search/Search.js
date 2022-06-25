8d1badafa265a16863f41b6b8f7ba683f56914e5
gracekrcx <kanmorning@gmail.com>
2022-06-25 15:08:55 +0800

    add image

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,15 +1,46 @@
 import { useState, useEffect, useCallback, useRef, useMemo } from 'react'
+import Image from 'next/image'
 import debounce from 'lodash/debounce'
 import { SearchWrapper, Input, Button } from './styled'
 import { useStore } from '../../context/store'
 // import useCountRenders from '../../hooks/useCountRenders'
+import { useRouter } from 'next/router'
+import searchIcon from '../../cache/images/searchIcon.jpeg'
 
 export default function Search() {
   const [keyword, setKeyword] = useState('')
   const { fetchRepositories } = useStore()
-  const debounceTime = 800
+  const debounceTime = 1000
+  const router = useRouter()
 
-  // useCountRenders()
+  useEffect(() => {
+    const handleRouteChangeError = (err, url) => {
+      if (err.cancelled) {
+        console.log(`Route to ${url} was cancelled!`)
+      }
+    }
+
+    const handleRouteChange = (url, { shallow }) => {
+      // console.log(
+      //   `App is changing to ${url} ${
+      //     shallow ? 'with' : 'without'
+      //   } shallow routing`
+      // )
+
+      const searchKeyword = url.replace('/?q=', '').trim()
+      setKeyword(searchKeyword)
+    }
+
+    router.events.on('routeChangeError', handleRouteChangeError)
+    router.events.on('routeChangeStart', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off('routeChangeError', handleRouteChangeError)
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [router.events])
 
   // (1) input change
   const handleChange = (e) => {
@@ -21,7 +52,10 @@ export default function Search() {
   const debouncedSearch = useCallback(
     debounce((searchKeyword) => {
       // (4) call github api
-      fetchRepositories(searchKeyword, 0)
+      // fetchRepositories(searchKeyword, 0)
+      router.push({
+        query: { q: searchKeyword },
+      })
     }, debounceTime),
     []
   )
@@ -39,6 +73,7 @@ export default function Search() {
 
   return (
     <SearchWrapper>
+      <Image src={searchIcon} alt="icon" width={320} height={200} />
       <Input
         type="text"
         placeholder="Find a repository…"
